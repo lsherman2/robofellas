cc0320ecf6bd114dc3ea7847112f68680fec40b8
lsherman2 <logansherman1@gmail.com>
2022-05-28 20:45:53 -0600

    Updates

---
+++ precache-manifest.70d113dc37a480c8a8816b8a1bcf87e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad84306e79cbe9fc4a2e",
+    "url": "/robofellas/static/css/main.9dad2201.chunk.css"
+  },
+  {
+    "revision": "ad84306e79cbe9fc4a2e",
+    "url": "/robofellas/static/js/main.ad84306e.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofellas/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "fa6c6e68f31a3e451899",
+    "url": "/robofellas/static/css/2.15c3f250.chunk.css"
+  },
+  {
+    "revision": "fa6c6e68f31a3e451899",
+    "url": "/robofellas/static/js/2.fa6c6e68.chunk.js"
+  },
+  {
+    "revision": "63339143048edff46a64",
+    "url": "/robofellas/static/js/runtime~main.63339143.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofellas/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "25d3ea5058e58941aede922ce565c616",
+    "url": "/robofellas/index.html"
+  }
+];
